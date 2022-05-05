9a2d3d54a4a94a827daf674ea675a513374b5101
efernov <efenov@gmail.com>
2022-05-05 19:26:42 +0200

    Segundo commit, hasta sesión 8, creado header-main-footer, traspasado header y footer del trabajo html-css, trabajado en casa

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+  },
+
+  {
+    path: "about",
+    loadChildren: () => import("src/app/about/about.module").then(m => m.AboutModule),
+  },
+  {
+    path: "not-found",
+    component: NotFoundComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "not-found",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

e029c5940143b921f22e070ba5acad35aa8dae9b
efernov <efernov@gmail.com>
2022-05-11 14:41:08 +0200

    Septimo Commit en clase modificando popover

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { PopoverModule} from 'ngx-bootstrap/popover';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,8 +14,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
-    PopoverModule.forRoot()
+    CoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/core/shell/footer/footer.component.ts
+++ frontend/src/app/core/shell/footer/footer.component.ts
@@ -5,9 +5,7 @@ import { createPopper } from '@popperjs/core';
 const popcorn = document.querySelector('#popcorn');
 const tooltip = document.querySelector('#tooltip');
 
-createPopper(popcorn, tooltip, {
-  placement: 'right-start',
-});
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -18,9 +16,7 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-      trigger: 'focus'
-    })
+
   }
 
 

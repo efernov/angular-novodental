38fa379aed88dc22d7f2d349216d80a86ae4b8fd
efernov <efernov@gmail.com>
2022-07-08 01:12:55 +0200

    poniendo variable en enviroment producion

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   logo: '../assets/logotipoG.jpg',
   host: 'https://novodental-api.herokuapp.com/api/',
+  hostPer: 'http://localhost:8080/',
   itemsPorPagina: 100
 };
